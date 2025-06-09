26fbeaac8572e6ef2e0ab0b0d431177beb70f478
aredwin11 <edwin.a@diggibyte.com>
2025-06-09 21:41:59 +0530

    added driver script to count and display word frequencies

---
+++ src/word_order/driver.py
@@ -0,0 +1,8 @@
+from util import count_words
+if __name__ == '__main__':
+    n = int(input())
+    words = [input() for _ in range(n)]
+
+    unique_count, counts = count_words(words)
+    print(unique_count)
+    print(' '.join(map(str, counts)))
