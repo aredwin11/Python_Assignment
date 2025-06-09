a37d444e31892bf03180c3594a44ac40d862c8e4
aredwin11 <edwin.a@diggibyte.com>
2025-06-09 10:29:00 +0530

    added time_delta function to calculate the difference in seconds

---
+++ src/time_delta/util.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+def time_delta(t1, t2):
+    date_format = "%a %d %b %Y %H:%M:%S %z"
+    time1 = datetime.strptime(t1, date_format)
+    time2 = datetime.strptime(t2, date_format)
+    return str(abs(int((time1 - time2).total_seconds())))
