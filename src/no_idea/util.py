a1a0b5f28c1aca29d1a5150a9f7759ba1b068d01
aredwin11 <edwin.a@diggibyte.com>
2025-06-09 21:47:37 +0530

    added function to calculate happiness based on set membership

---
+++ src/no_idea/util.py
@@ -0,0 +1,8 @@
+def calculate_happiness(arr, A, B):
+    happiness = 0
+    for num in arr:
+        if num in A:
+            happiness += 1
+        elif num in B:
+            happiness -= 1
+    return happiness
